41fb77a07220a08851f502b7b71645ac0d1b53c6
Erik Escoffier <hello@nerik.me>
2015-07-14 16:46:05 +0200

    [wip] templatize project

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,85 @@
 /*eslint-env node*/
 'use strict';
 
-var gulp = require('gulp'),
-browserify = require('gulp-browserify'),
-uglify = require('gulp-uglify'),
-browserSync = require('browser-sync');
+var path = require('path');
+var merge = require('merge-stream');
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var plugins = require('gulp-load-plugins')();
 
-var pipes = {
-	browserify: function() {
-        return gulp.src([ './src/js/dependencies.js' ])
-            .pipe(browserify({
-                insertGlobals: false,
-                debug: true
-            }))
-            .pipe(uglify())
-            .pipe(gulp.dest('./app/'));
+var config = {
+    currentProject: 'onusida',
+
+    projects: {
+        onusida: {
+            locales: ['fr-FR']
+        },
+        atlas: {
+            locales: ['fr-FR', 'mg-MG']
+        }
     }
 };
+
+function getLocalizedTemplateStream(locale) {
+    var data = {
+        trans: require(`./src/i18n/${locale}.json`)
+    };
+    data.trans.project = require(`./src/i18n/${config.currentProject}/${locale}.json`);
+
+    return gulp.src(`src/tpl/${config.currentProject}.html`)
+            .pipe(plugins.fileInclude())
+            .pipe(plugins.template(data, {
+                interpolate: /{{([\s\S]+?)}}/g,
+                evaluate: /{\?([\s\S]+?)\?}/g
+                // escape: /XXX/g
+            }))
+            .pipe(gulp.dest(path.join('dist', config.currentProject)));
+}
+
 
 gulp.task('default', function() {
     browserSync({
         server: {
-            baseDir: './app'
+            baseDir: './dist'
         },
         open: false,
         port: 3333
     });
 
-    gulp.watch('./app/**.*', function () {
+    gulp.watch('./src/**/*', ['build']);
+
+    gulp.watch('./dist/**/*', function () {
         browserSync.reload();
     });
 } );
 
 gulp.task('browserify', function () {
-    return pipes.browserify();
+    return gulp.src([ './src/js/dependencies.js' ])
+        .pipe(plugins.browserify({
+            insertGlobals: false,
+            debug: true
+        }))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('./app/'));
 });
+
+gulp.task('templates', function() {
+    var projectConfig = config.projects[config.currentProject];
+    var streams = merge();
+
+    projectConfig.locales.forEach(function(locale) {
+        streams.add(getLocalizedTemplateStream(locale));
+    });
+
+    return streams;
+});
+
+gulp.task('static', function() {
+    return gulp.src('./src/static/**/*')
+        .pipe(gulp.dest(path.join('dist', config.currentProject)));
+});
+
+
+gulp.task('build', ['templates', 'static']);
+
 
